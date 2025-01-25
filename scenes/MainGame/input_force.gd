a0eff63a63f25efab4e2191f159723ed2f6109f9
Egon Araujo <dev.egon.araujo@gmail.com>
2025-01-25 10:29:16 -0300

    player back to normal

--- scenes/MainGame/input_force.gd
+++ scenes/MainGame/input_force.gd
@@ -1,15 +1,14 @@
-extends Node2D
+extends RigidBody2D
 
 @export var power : float = 100
 @export var minRadius : float = 50
 @export var maxRadius : float = 200
 
-@onready var character := $RigidBody2D
 
 func _process(delta:float) -> void:
 	if Input.is_mouse_button_pressed(1):
 		#queue_redraw() #uncomment to get gizmos
-		var direction:Vector2 = character.global_position - get_global_mouse_position()
+		var direction:Vector2 = global_position - get_global_mouse_position()
 		
 		# Does not have an effect if bigger than max radius
 		if direction.length() > maxRadius:
@@ -21,7 +20,7 @@ func _process(delta:float) -> void:
 		# ease to make the force logarithmic instead of lin
 		var boostStrength = remap(distance, minRadius, maxRadius, 1, 0)
 		var boost = direction.normalized() * power * boostStrength
-		character.apply_central_force(boost)
+		apply_central_force(boost)
 
 func _draw() -> void:
 	return #comment to get gizmos
