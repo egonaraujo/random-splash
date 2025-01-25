2d8e49f5472ade3f0aac213d8b81d2b85f92facb
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-01-25 15:16:50 -0300

    FirstLevelPart

--- scenes/MainGame/input_force.gd
+++ scenes/MainGame/input_force.gd
@@ -1,12 +1,40 @@
 extends RigidBody2D
 
+@export_group("Click Input")
 @export var power : float = 100
 @export var minRadius : float = 50
 @export var maxRadius : float = 200
 
+@export_group("Oxygen Control")
+@export var oxygen : float = 100
+@export var oxygenLossPerSec: float = 0.5
+@export var oxygenLossPerImpact: float = 20
+
+@onready var originalSpriteScale:Vector2 = $Sprite.scale
+@onready var originalColliderScale:Vector2 = $CollisionShape2D.scale
+
+#Signals
+signal player_died()
+
+func _ready() -> void:
+	contact_monitor = true
+	max_contacts_reported = 1
+	
 
 func _process(delta:float) -> void:
-	if Input.is_mouse_button_pressed(1):
+	processOxygen(delta)
+	processInput()
+
+func processOxygen(delta:float) -> void:
+	oxygen -= oxygenLossPerSec * delta
+	if oxygen <= 0:
+		emit_signal("player_died")
+	var oxygenScale = remap(oxygen, 0, 100, 0.2, 1)
+	$Sprite.scale = oxygenScale * originalSpriteScale
+	$CollisionShape2D.scale = oxygenScale * originalColliderScale
+
+func processInput() -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		#queue_redraw() #uncomment to get gizmos
 		var direction:Vector2 = global_position - get_global_mouse_position()
 		
@@ -26,3 +54,10 @@ func _draw() -> void:
 	return #comment to get gizmos
 	draw_circle(get_global_mouse_position(), maxRadius, Color.DARK_BLUE)
 	draw_circle(get_global_mouse_position(), minRadius, Color.AQUA)
+
+
+func _on_body_entered(body: Node) -> void:
+	#How to detect walls?
+	print("Collided with " + body.name)
+	oxygen -= oxygenLossPerImpact
+	processOxygen(0)
