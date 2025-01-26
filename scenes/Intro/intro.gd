5994db5f53ef74948053827e992a80e480674ac4
Egon Araujo <dev.egon.araujo@gmail.com>
2025-01-26 13:07:18 -0300

    fix?

--- scenes/Intro/intro.gd
+++ scenes/Intro/intro.gd
@@ -8,5 +8,5 @@ func _on_next_button_down() -> void:
 			$Quadrinho1.hide()
 			$Quadrinho2.show()
 		1:
-			get_tree().change_scene_to_file("res://scenes/MainGame/MainGame.tscn")
+			get_tree().change_scene_to_file("res://scenes/Layout/GameLayout2.tscn")
 	state +=1
